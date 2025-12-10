6a1bfdad5e60c934546a77faa1417e92130ddc73
김석주 <seogjugim510@gmail.com>
2025-12-10 21:24:46 +0900

    test2

--- unity test/unity test/Program.cs
+++ unity test/unity test/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        System.Console.WriteLine("Hello, World!");
+    }
+}
